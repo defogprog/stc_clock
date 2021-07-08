0b2b1022a0216a707dbf242cc4c49897d46a4ec0
defog <psnk42@gmail.com>
2021-07-08 09:14:57 +0300

    [dev] Timer0 works

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdint.h>
 #include "stc12c.h"
 
-#define DS1 (1<<0)
-#define DS2 (1<<1)
-#define DS3 (1<<2)
-#define DS4 (1<<3)
+#define DS1 P3_0
+#define DS2 P3_1
+#define DS3 P3_2
+#define DS4 P3_3
+
+#define DIG_PORT P1
+#define DIG_DOT  P1_0
+
+#define FOSC    12000000
+#define T1MS    (65536 - FOSC/12/1000)
+#define T2MS    (65536 - FOSC/12/500)
+#define T5MS    (65536 - FOSC/12/200)
+#define T10MS   (65536 - FOSC/12/100)
 
 #define countof(_a) (sizeof(_a)/sizeof(*_a))
 
@@ -16,22 +25,75 @@ const uint8_t digits[] = {
     0x02,0x76,0x58,         // -, H, L,
 };
 
+/**
+ * FUNCTIONS
+ */
+static void led_set_digit(uint8_t dig, uint8_t dot) {
+    if (dig >= countof(digits)) {
+        return;
+    }
+    DIG_PORT = digits[dig];
+    if (dot) DIG_DOT = 1;
+}
+
+static void init_timers(void) {
+    // Init Timer0
+    TMOD = 0x01;
+    TL0 = T5MS;
+    TH0 = T5MS>>8;
+    TR0 = 1;
+    ET0 = 1;
+}
+
+/*
+ * ISRs
+ */
+void T0_ISR(void) __interrupt (1) {
+    static uint8_t position = 0;
+    uint8_t dot = 0;
+
+    TL0 = T5MS;
+    TH0 = T5MS>>8;
+
+    static uint8_t times = 0;
+
+        DS1 = DS2 = DS3 = DS4 = 1;
+        if (position == 1 && times <= 50) dot = 1;
+        led_set_digit(position, dot);
+        if (position == 0) DS1 = 0; else
+        if (position == 1) DS2 = 0; else
+        if (position == 2) DS3 = 0; else
+        if (position == 3) DS4 = 0;
+        if (++position == 4) {
+            position = 0;
+            if (++times == 100) {
+                times = 0;
+            }
+     }
+}
+
+/*
+ * MAIN
+ */
 int main(void) {
     uint8_t d = 0;
     uint8_t num = 0;
-    P3 = ~(DS1);
-        
+    
+    init_timers();
+
+    // Enable interrupts globally
+    EA = 1;
+
     while (1) {
-        P3 = (DS1 | DS2 | DS3 | DS4);
-        for (num=0; num<4; num++) {
-            P3 = ~(1<<num);
-            for (d = 0; d < countof(digits); d++) {
-                P1 = digits[d];
-                volatile uint16_t delay = (uint16_t)(-1);
-                while (--delay);
-                while (--delay);
-            }
-        }
+        // P3 = (DS1 | DS2 | DS3 | DS4);
+        // for (num=0; num<4; num++) {
+        //     P3 = ~(1<<num);
+        //     for (d = 0; d < countof(digits); d++) {
+        //         P1 = digits[d];
+        //         volatile uint16_t delay = (uint16_t)(-1);
+        //         while (--delay);
+        //         while (--delay);
+        //     }
+        // }
     }
-    return 0;
 }
