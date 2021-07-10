c0f60aecec10a91e7d6ee589c6bb3e97b29edb6b
defog <psnk42@gmail.com>
2021-07-10 10:33:02 +0300

    [dev] Refactor stc12c.h

--- include/stc12c.h
+++ include/stc12c.h
@@ -12,89 +12,103 @@
 #define ADC_SPI_VEC     5
 #define PCA_LVD_VEC     6
 
-#define __SFR_P0        0x80
-#define __SFR_SP        0x81
-#define __SFR_DPL       0x82
-#define __SFR_DPH       0x83
-#define __SFR_PCON      0x87
-#define __SFR_TCON      0x88
-#define __SFR_TMOD      0x89
-#define __SFR_TL0       0x8A
-#define __SFR_TL1       0x8B
-#define __SFR_TH0       0x8C
-#define __SFR_TH1       0x8D
-#define __SFR_AUXR      0x8E
-#define __SFR_WAKE_CLKO 0x8F
+#define __SFR_P0            0x80
+#define __SFR_SP            0x81
+#define __SFR_DPL           0x82
+#define __SFR_DPH           0x83
+#define __SFR_PCON          0x87
+#define __SFR_TCON          0x88
+#define __SFR_TMOD          0x89
+    #define TMOD_T0_M0      (1<<0)
+    #define TMOD_T0_M1      (1<<1)
+    #define TMOD_T0_CT      (1<<2)
+    #define TMOD_T0_GATE    (1<<3)
+    #define TMOD_T1_M0      (1<<4)
+    #define TMOD_T1_M1      (1<<5)
+    #define TMOD_T1_CT      (1<<6)
+    #define TMOD_T1_GATE    (1<<7)
+#define __SFR_TL0           0x8A
+#define __SFR_TL1           0x8B
+#define __SFR_TH0           0x8C
+#define __SFR_TH1           0x8D
+#define __SFR_AUXR          0x8E
+    #define AUXR_ELVDI      (1<<2)
+    #define AUXR_ESPI       (1<<3)
+    #define AUXR_EADCI      (1<<4)
+    #define AUXR_UART_M0x6  (1<<5)
+    #define AUXR_T1x12      (1<<6)
+    #define AUXR_T0x12      (1<<7)
+#define __SFR_WAKE_CLKO     0x8F
 
-#define __SFR_P1        0x90
-#define __SFR_P1M1      0x91
-#define __SFR_P1M0      0x92
-#define __SFR_P0M1      0x93
-#define __SFR_P0M0      0x94
-#define __SFR_P2M1      0x95
-#define __SFR_P2M0      0x96
-#define __SFR_CLK_DIV   0x97
-#define __SFR_SCON      0x98
-#define __SFR_SBUF      0x99
-#define __SFR_S2CON     0x9A
-#define __SFR_S2BUF     0x9B
-#define __SFR_BRT       0x9C
-#define __SFR_P1ASF     0x9D
+#define __SFR_P1            0x90
+#define __SFR_P1M1          0x91
+#define __SFR_P1M0          0x92
+#define __SFR_P0M1          0x93
+#define __SFR_P0M0          0x94
+#define __SFR_P2M1          0x95
+#define __SFR_P2M0          0x96
+#define __SFR_CLK_DIV       0x97
+#define __SFR_SCON          0x98
+#define __SFR_SBUF          0x99
+#define __SFR_S2CON         0x9A
+#define __SFR_S2BUF         0x9B
+#define __SFR_BRT           0x9C
+#define __SFR_P1ASF         0x9D
 
-#define __SFR_P2        0xA0
-#define __SFR_BUS_SPEED 0xA1
-#define __SFR_AUXR1     0xA2
-#define __SFR_IE        0xA8
-#define __SFR_SADDR     0xA9
-#define __SFR_IE2       0xAF
+#define __SFR_P2            0xA0
+#define __SFR_BUS_SPEED     0xA1
+#define __SFR_AUXR1         0xA2
+#define __SFR_IE            0xA8
+#define __SFR_SADDR         0xA9
+#define __SFR_IE2           0xAF
 
-#define __SFR_P3        0xB0
-#define __SFR_P3M1      0xB1
-#define __SFR_P3M0      0xB2
-#define __SFR_P4M1      0xB3
-#define __SFR_P4M0      0xB4
-#define __SFR_IP2       0xB5
-#define __SFR_IPH2      0xB6
-#define __SFR_IPH       0xB7
-#define __SFR_IP        0xB8
-#define __SFR_SADEN     0xB9
-#define __SFR_P4SW      0xBB
-#define __SFR_ADC_CONTR 0xBC
-#define __SFR_ADC_RES   0xBD
-#define __SFR_ADC_RESL  0xBE
+#define __SFR_P3            0xB0
+#define __SFR_P3M1          0xB1
+#define __SFR_P3M0          0xB2
+#define __SFR_P4M1          0xB3
+#define __SFR_P4M0          0xB4
+#define __SFR_IP2           0xB5
+#define __SFR_IPH2          0xB6
+#define __SFR_IPH           0xB7
+#define __SFR_IP            0xB8
+#define __SFR_SADEN         0xB9
+#define __SFR_P4SW          0xBB
+#define __SFR_ADC_CONTR     0xBC
+#define __SFR_ADC_RES       0xBD
+#define __SFR_ADC_RESL      0xBE
 
-#define __SFR_P4        0xC0
-#define __SFR_WDT_CONTR 0xC1
-#define __SFR_IAP_DATA  0xC2
-#define __SFR_IAP_ADDRH 0xC3
-#define __SFR_IAP_ADDRL 0xC4
-#define __SFR_IAP_CMD   0xC5
-#define __SFR_IAP_TRIG  0xC6
-#define __SFR_IAP_CONTR 0xC7
-#define __SFR_P5        0xC8
-#define __SFR_P5M1      0xC9
-#define __SFR_P5M0      0xCA
-#define __SFR_SPSTAT    0xCD
-#define __SFR_SPCTL     0xCE
-#define __SFR_SPDAT     0xCF
+#define __SFR_P4            0xC0
+#define __SFR_WDT_CONTR     0xC1
+#define __SFR_IAP_DATA      0xC2
+#define __SFR_IAP_ADDRH     0xC3
+#define __SFR_IAP_ADDRL     0xC4
+#define __SFR_IAP_CMD       0xC5
+#define __SFR_IAP_TRIG      0xC6
+#define __SFR_IAP_CONTR     0xC7
+#define __SFR_P5            0xC8
+#define __SFR_P5M1          0xC9
+#define __SFR_P5M0          0xCA
+#define __SFR_SPSTAT        0xCD
+#define __SFR_SPCTL         0xCE
+#define __SFR_SPDAT         0xCF
 
-#define __SFR_PSW       0xD0
-#define __SFR_CCON      0xD8
-#define __SFR_CMOD      0xD9
-#define __SFR_CCAPM0    0xDA
-#define __SFR_CCAPM1    0xDB
+#define __SFR_PSW           0xD0
+#define __SFR_CCON          0xD8
+#define __SFR_CMOD          0xD9
+#define __SFR_CCAPM0        0xDA
+#define __SFR_CCAPM1        0xDB
 
-#define __SFR_ACC       0xE0
-#define __SFR_CL        0xE9
-#define __SFR_CCAP0L    0xEA
-#define __SFR_CCAP1L    0xEB
+#define __SFR_ACC           0xE0
+#define __SFR_CL            0xE9
+#define __SFR_CCAP0L        0xEA
+#define __SFR_CCAP1L        0xEB
 
-#define __SFR_B         0xF0
-#define __SFR_PCA_PWM0  0xF2
-#define __SFR_PCA_PWM1  0xF3
-#define __SFR_CH        0xF9
-#define __SFR_CCAP0H    0xFA
-#define __SFR_CCAP1H    0xFB
+#define __SFR_B             0xF0
+#define __SFR_PCA_PWM0      0xF2
+#define __SFR_PCA_PWM1      0xF3
+#define __SFR_CH            0xF9
+#define __SFR_CCAP0H        0xFA
+#define __SFR_CCAP1H        0xFB
 
 
 __sfr __at(0xE0) ACC;
@@ -176,14 +190,14 @@ __sfr __at(0xFA) CCAP0H;
 __sfr __at(0xFB) CCAP1H;
 
 // 0x88
-__sbit __at(__SFR_TCON^7) TF1;
-__sbit __at(__SFR_TCON^6) TR1;
-__sbit __at(__SFR_TCON^5) TF0;
-__sbit __at(__SFR_TCON^4) TR0;
-__sbit __at(__SFR_TCON^3) IE1;
-__sbit __at(__SFR_TCON^2) IT1;
-__sbit __at(__SFR_TCON^1) IE0;
-__sbit __at(__SFR_TCON^0) IT0;
+__sbit __at(__SFR_TCON^7) TCON_TF1;
+__sbit __at(__SFR_TCON^6) TCON_TR1;
+__sbit __at(__SFR_TCON^5) TCON_TF0;
+__sbit __at(__SFR_TCON^4) TCON_TR0;
+__sbit __at(__SFR_TCON^3) TCON_IE1;
+__sbit __at(__SFR_TCON^2) TCON_IT1;
+__sbit __at(__SFR_TCON^1) TCON_IE0;
+__sbit __at(__SFR_TCON^0) TCON_IT0;
 
 // 0x90
 __sbit __at(__SFR_P1^7) P1_7;
@@ -196,24 +210,24 @@ __sbit __at(__SFR_P1^1) P1_1;
 __sbit __at(__SFR_P1^0) P1_0;
 
 // 0x98
-__sbit __at(__SFR_SCON^7) SM0_FE;
-__sbit __at(__SFR_SCON^6) SM1;
-__sbit __at(__SFR_SCON^5) SM2;
-__sbit __at(__SFR_SCON^4) REN;
-__sbit __at(__SFR_SCON^3) TB8;
-__sbit __at(__SFR_SCON^2) RB8;
-__sbit __at(__SFR_SCON^1) TI;
-__sbit __at(__SFR_SCON^0) RI;
+__sbit __at(__SFR_SCON^7) SCONSM0_FE;
+__sbit __at(__SFR_SCON^6) SCONSM1;
+__sbit __at(__SFR_SCON^5) SCONSM2;
+__sbit __at(__SFR_SCON^4) SCONREN;
+__sbit __at(__SFR_SCON^3) SCONTB8;
+__sbit __at(__SFR_SCON^2) SCONRB8;
+__sbit __at(__SFR_SCON^1) SCONTI;
+__sbit __at(__SFR_SCON^0) SCONRI;
 
 // 0xA8
-__sbit __at(__SFR_IE^7) EA;
-__sbit __at(__SFR_IE^6) ELVD;
-__sbit __at(__SFR_IE^5) EADC;
-__sbit __at(__SFR_IE^4) ES;
-__sbit __at(__SFR_IE^3) ET1;
-__sbit __at(__SFR_IE^2) EX1;
-__sbit __at(__SFR_IE^1) ET0;
-__sbit __at(__SFR_IE^0) EX0;
+__sbit __at(__SFR_IE^7) IE_EA;
+__sbit __at(__SFR_IE^6) IE_ELVD;
+__sbit __at(__SFR_IE^5) IE_EADC;
+__sbit __at(__SFR_IE^4) IE_ES;
+__sbit __at(__SFR_IE^3) IE_ET1;
+__sbit __at(__SFR_IE^2) IE_EX1;
+__sbit __at(__SFR_IE^1) IE_ET0;
+__sbit __at(__SFR_IE^0) IE_EX0;
 
 // 0xB0
 __sbit __at(__SFR_P3^7) P3_7;
@@ -226,30 +240,30 @@ __sbit __at(__SFR_P3^1) P3_1;
 __sbit __at(__SFR_P3^0) P3_0;
 
 // 0xB8
-__sbit __at(__SFR_IP^7) PPCA;
-__sbit __at(__SFR_IP^6) PLVD;
-__sbit __at(__SFR_IP^5) PADC;
-__sbit __at(__SFR_IP^4) PS;
-__sbit __at(__SFR_IP^3) PT1;
-__sbit __at(__SFR_IP^2) PX1;
-__sbit __at(__SFR_IP^1) PT0;
-__sbit __at(__SFR_IP^0) PX0;
+__sbit __at(__SFR_IP^7) IP_PPCA;
+__sbit __at(__SFR_IP^6) IP_PLVD;
+__sbit __at(__SFR_IP^5) IP_PADC;
+__sbit __at(__SFR_IP^4) IP_PS;
+__sbit __at(__SFR_IP^3) IP_PT1;
+__sbit __at(__SFR_IP^2) IP_PX1;
+__sbit __at(__SFR_IP^1) IP_PT0;
+__sbit __at(__SFR_IP^0) IP_PX0;
 
 // 0xD0
-__sbit __at(__SFR_PSW^7) CY;
-__sbit __at(__SFR_PSW^6) AC;
-__sbit __at(__SFR_PSW^5) F0;
-__sbit __at(__SFR_PSW^4) RS1;
-__sbit __at(__SFR_PSW^3) RS0;
-__sbit __at(__SFR_PSW^2) OV;
-__sbit __at(__SFR_PSW^1) F1;
-__sbit __at(__SFR_PSW^0) P;
+__sbit __at(__SFR_PSW^7) PSW_CY;
+__sbit __at(__SFR_PSW^6) PSW_AC;
+__sbit __at(__SFR_PSW^5) PSW_F0;
+__sbit __at(__SFR_PSW^4) PSW_RS1;
+__sbit __at(__SFR_PSW^3) PSW_RS0;
+__sbit __at(__SFR_PSW^2) PSW_OV;
+__sbit __at(__SFR_PSW^1) PSW_F1;
+__sbit __at(__SFR_PSW^0) PSW_P;
 
 // 0xD8
-__sbit __at(__SFR_CCON^7) CF;
-__sbit __at(__SFR_CCON^6) CR;
-__sbit __at(__SFR_CCON^1) CCF1;
-__sbit __at(__SFR_CCON^0) CCF0;
+__sbit __at(__SFR_CCON^7) CCON_CF;
+__sbit __at(__SFR_CCON^6) CCON_CR;
+__sbit __at(__SFR_CCON^1) CCON_CCF1;
+__sbit __at(__SFR_CCON^0) CCON_CCF0;
 
 // 0xE0
 __sbit __at(__SFR_ACC^7) ACC_7;
