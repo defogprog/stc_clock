2d4c33772b0c573200472966222374bfa5241c6e
defog <psnk42@gmail.com>
2021-07-10 11:14:15 +0300

    [dev] Blinking and dot work

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define DIG_PORT        P1
 #define DIG_DOT         P1_0
 
-#define BLINK_PERIOD    250
+#define BLINK_PERIOD    500
 
 #define FOSC            12000000
 #define T1MS            (65536 - FOSC/(12*1000))
@@ -24,8 +24,8 @@ enum {
     _4,_5,_6,_7,
     _8,_9,_A,_B,
     _C,_D,_E,_F,
-    _DASH,_H,_L,_I,
-    __,
+    _H,_L,_I,_P,
+    _DASH,__,
 };
 
 const static uint8_t symbols[] = {
@@ -33,11 +33,33 @@ const static uint8_t symbols[] = {
     0x66,0xCE,0xDE,0xA4,    // 4, 5, 6, 7,
     0xFE,0xEE,0xF6,0x5E,    // 8, 9, A, b,
     0xD8,0x3E,0xDA,0xD2,    // C, d, E, F,
-    0x02,0x76,0x58,0x50,    // -, H, L, I,
-    0x08,                   // _
+    0x76,0x58,0x50,0xF2,    // H, L, I,
+    0x02,0x08,              // -, _.
 };
 
-volatile static uint8_t digits[4] = { _L,_0,_H,_I};
+typedef enum  {
+    DOT_OFF,
+    DOT_ON,
+    DOT_BLINK,
+} dot_state;
+
+typedef struct _digit_t {
+    uint8_t symb;
+    uint8_t blink : 1;
+    dot_state dot : 2;
+} digit_t;
+
+volatile static digit_t digits[4] = { 
+    [0].symb = _1,
+    [1].symb = _2,
+    [2].symb = _P,
+    [3].symb = _P,
+    [1].dot = DOT_BLINK,
+    [2].dot = DOT_ON,
+    [3].dot = DOT_BLINK,
+    [0].blink = 1,
+    [2].blink = 1,
+};
 
 /**
  * FUNCTIONS
@@ -46,8 +68,8 @@ static void init_timers(void) {
     // Init Timer0
     AUXR &= ~AUXR_T0x12;                // SYSclk/12
     TMOD = TMOD_T1_M0;                  // 16-bit Timer mode
-    TL0 = T10MS;
-    TH0 = T10MS>>8;
+    TL0 = T10MS & 0xFF;
+    TH0 = T10MS >> 8;
     TCON_TR0 = 1;                       // Enable Tim0
     IE_ET0 = 1;                         // Enable Tim0 Interrupts
 }
@@ -57,17 +79,27 @@ static void init_timers(void) {
  */
 void T0_ISR(void) __interrupt (TIM0_VEC) {
     static uint8_t position = 0;
-    uint8_t dot = 0;
 
-    TL0 = T10MS;
-    TH0 = T10MS>>8;
+    TL0 = T10MS & 0xFF;
+    TH0 = T10MS >> 8;
 
     static uint16_t times = 0;
 
     P3 |= 0x0F;                         // Turn off digits
-    if (position == 1 && times <= (BLINK_PERIOD/2))
-        dot = 1;
-    DIG_PORT = symbols[digits[position]];
+
+    if (digits[position].blink && times > (BLINK_PERIOD/2))
+        DIG_PORT = 0;
+    else
+        DIG_PORT = symbols[digits[position].symb];
+
+    switch (digits[position].dot) {
+        case DOT_BLINK:
+            if (times > (BLINK_PERIOD/2))
+                break;
+        case DOT_ON:
+            DIG_PORT |= (1<<0);
+            break;
+    }
     P3 &= ~(1<<position);               // Turn on digits
 
     if (++position == 4) {
