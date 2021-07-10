8f4c1681920d26570ddffa514db3313e0e25cb12
defog <psnk42@gmail.com>
2021-07-10 19:37:49 +0300

    [dev] Optimize a little, still lack of memory

--- main.c
+++ main.c
@@ -15,13 +15,13 @@
  * @brief   Defines
  */
 #define DIGITS_PORT             P1
-#define BLINK_PERIOD            250
+#define BUTTON_MODE             P3_4
+#define BUTTON_SET              P3_5
 #define _TIM_PERIOD(_ms)        (-_ms##000 - 1)
 #define TIM_PERIOD(_ms)         _TIM_PERIOD(_ms)
 #define LED_SCAN_PERIOD_MS      1
 #define BUTTON_SCAN_PERIOD_MS   50
-#define BUTTON_MODE             P3_4
-#define BUTTON_SET              P3_5
+#define BLINK_PERIOD            250
 
 #define countof(_a) (sizeof(_a)/sizeof(*_a))
 
@@ -70,7 +70,6 @@ typedef struct {
 typedef struct {
     uint8_t day;
     uint8_t month;
-    uint8_t year;
 } date_t;
 
 typedef struct {
@@ -80,18 +79,20 @@ typedef struct {
 } alarm_t;
 
 typedef enum {
-    MODE_CLOCK,
-    MODE_SECONDS,
+    MODE_NORMAL         = 0x10,
     MODE_DATE,
-    MODE_YEAR,
-    MODE_ALARM,
+    MODE_SECONDS,
+    MODE_NORMAL_END,
+    MODE_SET            = 0x20,
     MODE_SET_HOUR,
     MODE_SET_MIN,
     MODE_SET_DAY,
     MODE_SET_MON,
-    MODE_SET_YEAR,
-    MODE_SET_ALM_HOUR,
-    MODE_SET_ALM_MIN,
+    MODE_SET_END,
+    MODE_ALARM          = 0x40,
+    MODE_ALARM_HOUR,
+    MODE_ALARM_MIN,
+    MODE_ALARM_STOP,
 } clock_mode_t;
 
 /**
@@ -110,6 +111,17 @@ static const static uint8_t symbols[] = {
     0x76,0x58,0x50,0xF2,    // H, L, I,
     0x02,0x08,              // -, _.
 };
+static const uint8_t *params[MODE_SET_END - MODE_SET - 1] = {
+    &(time.hours),
+    &(time.minutes),
+    &(date.day),
+    &(date.month)
+};
+
+static const uint8_t limits[MODE_SET_END - MODE_SET - 1] = {
+    24, 60, 31, 12,
+};
+
 /**
  * @brief   Variables
  */
@@ -163,11 +175,11 @@ static uint8_t ones(uint8_t num) {
  * @param limit     
  * @return uint8_t  0 if no overflow, 1 of overflowed
  */
-static uint8_t inc_with_ovf(uint8_t num, uint8_t limit) {
-    if (++num == limit) {
-        num = 0;
+static void inc_with_ovf(uint8_t *num, uint8_t limit) {
+    // if (!num) return;
+    if (++(*num) == limit) {
+        (*num) = 0;
     }
-    return num;
 }
 
 /**
@@ -183,26 +195,22 @@ void T0_ISR(void) __interrupt (TIM0_VEC) {
     // Time and calendar
     if (++ticks == 1000) {
         ticks = 0;
-        if (++time.seconds > 59) {
+        if (++time.seconds == 60) {
             time.seconds = 0;
-            if (++time.minutes > 59) {
+            if (++time.minutes == 60) {
                 time.minutes = 0;
-                if (++time.hours > 23) {
+                if (++time.hours == 24) {
                     time.hours = 0;
-                    if (++date.day > days_in_month[date.month]) {
-                        date.day = 1;
-                        if (++date.month > 12) {
-                            date.month = 1;
-                            if (++date.year > 99) {
-                                date.year = 0;
-                            }
+                    if (++date.day == days_in_month[date.month] - 1) {
+                        date.day = 0;
+                        if (++date.month == 11) {
+                            date.month = 0;
                         }
                     }
                 }
             }
         }
     }
-
     uint8_t i;
     for (i=0; i<4; i++) {
         digits[i].symb = 0;
@@ -212,14 +220,14 @@ void T0_ISR(void) __interrupt (TIM0_VEC) {
 
     switch (clock_mode) {
         // Display "HR:MN" with blinking colon
-        case MODE_CLOCK:
+        case MODE_NORMAL:
             digits[0].symb = symbols[dec(time.hours)];
             digits[1].symb = symbols[ones(time.hours)];
             digits[1].dot = DOT_BLINK;
             digits[2].symb = symbols[dec(time.minutes)];
             digits[3].symb = symbols[ones(time.minutes)];
             break;
-        // Display "  :SE" with solid colon
+        // Display "  :SE" with blinking colon
         case MODE_SECONDS:
             digits[1].dot = DOT_BLINK;
             digits[2].symb = symbols[dec(time.seconds)];
@@ -232,13 +240,6 @@ void T0_ISR(void) __interrupt (TIM0_VEC) {
             digits[1].symb = symbols[ones(date.day)];
             digits[2].symb = symbols[dec(date.month)];
             digits[3].symb = symbols[ones(date.month)];
-            break;
-        // Display "YYYY"
-        case MODE_YEAR:
-            digits[0].symb = symbols[_2];
-            digits[1].symb = symbols[_0];
-            digits[2].symb = symbols[dec(date.year)];
-            digits[3].symb = symbols[ones(date.year)];
             break;
         // Display "HR:MN" with solid colon
         case MODE_ALARM:
@@ -288,17 +289,6 @@ void T0_ISR(void) __interrupt (TIM0_VEC) {
             digits[3].symb = symbols[ones(date.month)];
             digits[3].blink = 1;
             break;
-        case MODE_SET_YEAR:
-            digits[0].symb = symbols[_2];
-            digits[0].blink = 1;
-            digits[1].symb = symbols[_0];
-            digits[1].blink = 1;
-            digits[2].symb = symbols[dec(date.year)];
-            digits[2].blink = 1;
-            digits[3].symb = symbols[ones(date.year)];
-            digits[3].blink = 1;
- 
-            break;
         default:
             break;
     }
@@ -346,30 +336,10 @@ void T1_ISR(void) __interrupt(TIM1_VEC) {
         switch (buttons_state.mode) {
             case BUT_RELEASED:
                 buttons_state.mode = BUT_PRESSED;
-                switch (clock_mode) {
-                    // These modes are set with long press
-                    case MODE_SET_HOUR:
-                        timeout = 100;
-                        clock_mode = MODE_SET_MIN;
-                        break;
-                    case MODE_SET_MIN:
-                        timeout = 100;
-                        clock_mode = MODE_SET_DAY;
-                        break;
-                    case MODE_SET_DAY:
-                        timeout = 100;
-                        clock_mode = MODE_SET_MON;
-                        break;
-                    case MODE_SET_MON:
-                        timeout = 100;
-                        clock_mode = MODE_SET_YEAR;
-                        break;
-                    case MODE_SET_YEAR:
-                        timeout = 100;
-                        clock_mode = MODE_CLOCK;
-                        break;
-                    default:
-                        break;
+                if (clock_mode & MODE_SET) {
+                    if (++clock_mode == MODE_SET_END) {
+                        clock_mode = MODE_NORMAL;
+                    }
                 }
                 break;
             case BUT_PRESSED:
@@ -379,13 +349,14 @@ void T1_ISR(void) __interrupt(TIM1_VEC) {
                 }
                 break;
             case BUT_SHORT_PRESS:
-                if (clock_mode == MODE_CLOCK ||
-                    clock_mode == MODE_DATE) {
-                    clock_mode = MODE_SET_HOUR;
-                } else if (clock_mode == MODE_SECONDS) {
-                    time.seconds = 0;
+                if (clock_mode & MODE_NORMAL) {
+                    if (clock_mode == MODE_SECONDS) {
+                        time.seconds = 0;
+                    } else {
+                        clock_mode = MODE_SET_HOUR;
+                    }
                 } else {
-                    clock_mode = MODE_CLOCK;
+                    clock_mode = MODE_NORMAL;
                 }
                 buttons_state.mode = BUT_LONG_PRESS;
                 break;
@@ -401,52 +372,46 @@ void T1_ISR(void) __interrupt(TIM1_VEC) {
 
     // SET button state machine
     if (BUTTON_SET == 0) {
+        uint8_t *param;
+        uint8_t limit;
+
         switch (buttons_state.set) {
             case BUT_RELEASED:
                 buttons_state.set = BUT_PRESSED;
-                switch (clock_mode) {
-                    // These modeas are set cyclically with short press
-                    case MODE_CLOCK:
-                        clock_mode = MODE_DATE;
-                        timeout = 20;
-                        break;
-                    case MODE_DATE:
-                        clock_mode = MODE_SECONDS;
-                        break;
-                    case MODE_SECONDS:
-                        clock_mode = MODE_CLOCK;
-                        break;
-                    
-                    case MODE_SET_HOUR:
-                        time.hours = inc_with_ovf(time.hours, 24);
-                        break;
-                    case MODE_SET_MIN:
-                        time.minutes = inc_with_ovf(time.minutes, 60);
-                    default:
-                        break;
+                if (clock_mode & MODE_NORMAL) {
+                    if (++clock_mode == MODE_NORMAL_END) {
+                        clock_mode = MODE_NORMAL;
+                    }
+                }
+                if (clock_mode == MODE_DATE) {
+                    timeout = 20;
+                }
+                if (clock_mode == MODE_SECONDS) {
+                    timeout = 0;
+                }
+                if (clock_mode & MODE_SET) {
+                    param = params[clock_mode - MODE_SET - 1];
+                    limit = limits[clock_mode - MODE_SET - 1];
+                    inc_with_ovf(param, limit);
                 }
                 break;
             case BUT_PRESSED:
                 if (++press_duration.set == 20) {
                     press_duration.set = 0;
-                    buttons_state.set = BUT_SHORT_PRESS;
+                    buttons_state.set = BUT_LONG_PRESS;
                 }
                 break;
             case BUT_SHORT_PRESS:
-                // if (clock_mode == MODE_SET_HOUR) {
-                //     time.hours = inc_with_ovf(time.hours, 24);
-                // }
-                // switch (clock_mode) {
-                //     case MODE_SET_HOUR:
-                //         time.hours = inc_with_ovf(time.hours, 24);
-                //         break;
-                //     case MODE_SET_MIN:
-                //         time.minutes = inc_with_ovf(time.minutes, 60);
-                //     default:
-                //         break;
-                // }
                 break;
             case BUT_LONG_PRESS:
+                if (clock_mode & MODE_SET) {
+                    param = params[clock_mode - MODE_SET - 1];
+                    limit = limits[clock_mode - MODE_SET - 1];
+                    inc_with_ovf(param, limit);
+                    // if (clock_mode > MODE_SET_MIN) {
+                    //     (*param)++;
+                    // }
+                }
                 break;
             default:
                 break;
@@ -458,7 +423,7 @@ void T1_ISR(void) __interrupt(TIM1_VEC) {
 
     if (timeout != 0) {
         if (--timeout == 0) {
-            clock_mode = MODE_CLOCK;
+            clock_mode = MODE_NORMAL;
         }
     }
 }
@@ -476,7 +441,6 @@ int main(void) {
     // Init date
     date.day = 1;
     date.month = 1;
-    date.year = 20;
 
     // Init alarm
     alarm[0].hour = 13;
@@ -484,7 +448,7 @@ int main(void) {
     alarm[0].on = 0;
 
     // Set default clock mode
-    clock_mode = MODE_CLOCK;
+    clock_mode = MODE_NORMAL;
 
     init_timers();
 
